066e340b1bf14bcd72c60a85fe853757be1586f8
Abas Farah <abasnfarah@gmail.com>
2024-03-01 17:58:17 -0600

    fix: images not being available on email

--- src/emails/welcome.tsx
+++ src/emails/welcome.tsx
@@ -133,7 +133,7 @@ export const EzylendsWelcomeEmail = ({
       >
         <Body className="bg-offwhite font-sans text-base">
           <Img
-            src={`${baseUrl}/public/ezLogo.png`}
+            src={`${baseUrl}/static/ezLogo.png`}
             width="228"
             height="128"
             alt="EzyLends"
