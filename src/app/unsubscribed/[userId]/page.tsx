61e7e3f9555b873ba847999d17b34228a72726c0
Abas Farah <abasnfarah@gmail.com>
2024-03-01 18:28:04 -0600

    fix: unsubscribed

--- src/app/unsubscribed/[userId]/page.tsx
+++ src/app/unsubscribed/[userId]/page.tsx
@@ -1,3 +1,4 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import { api } from '@/trpc/server'
 
 interface userIdProps {
@@ -7,13 +8,22 @@ interface userIdProps {
 }
 
 const UnsubscribeUser = async ({ params }: userIdProps) => {
-  console.log(params.userid)
-  const user = api.user.getUserById.query({ id: params.userid })
-  console.log(user)
+  const users = await api.user.getUserById.query({ id: params.userid })
+  const user = users[0]
+
+  if (user) {
+    await api.user.unsubscribeEmail.mutate({
+      id: user.id
+    })
+  }
 
   return (
-    <div>
-      <div>{params.userid}</div>
+    <div className="flex flex-col items-center justify-center pb-12 md:pb-20 lg:flex-row lg:pb-32">
+      <MaxWidthWrapper className="mb-6 mt-14 flex w-full flex-col items-center justify-center text-center md:mb-12 md:mt-16 lg:mt-28 lg:w-1/2">
+        <p className="mt-4 max-w-prose text-center text-base dark:text-zinc-50 md:mt-5 md:text-lg">
+          You have been unsubscribed
+        </p>
+      </MaxWidthWrapper>
     </div>
   )
 }
